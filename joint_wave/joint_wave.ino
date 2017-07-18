bc99288f48cf02c4de8c363b9f57db219b598251
Katya Soltan <kattsoltan@gmail.com>
2017-07-18 17:15:47 -0400

    Fixed bug that incorrectly updated directions of joints because of rounding problem in XOR statement. The outputs now are correct, but fish is still not moving the tail and joint3, even with a new H-Bridge.

--- joint_wave/joint_wave.ino
+++ joint_wave/joint_wave.ino
@@ -1,23 +1,29 @@
-const int NUM_JOINTS = 2;
+const int NUM_JOINTS = 4;
 uint8_t joint_to_move = 0; // 0-head, 1-joint2, 2-joint3, 3-tail
 uint8_t joint_directions = B0000; // all heads start pointing in left direction (tail, joint3, joint2, head)
 uint8_t LEFT = 0;
 uint8_t RIGHT = 1;
 
 // ARDUINO PINS FOR MICs
-uint8_t tail_r = 3;
-uint8_t tail_l = 5;
-uint8_t fin_r = 16; // A2
-uint8_t fin_l = 17; // A3
-uint8_t joint_pins[NUM_JOINTS][2] = {{tail_l, tail_r}, {fin_l, fin_r}};
+uint8_t head_r = 3;
+uint8_t head_l = 5;
+uint8_t joint2_r = 6;
+uint8_t joint2_l = 9;
+uint8_t joint3_r = 10;
+uint8_t joint3_l = 11;
+uint8_t tail_r = 16; // A2
+uint8_t tail_l = 17; // A3
+//uint8_t joint_pins[NUM_JOINTS][2] = {{joint3_l, joint3_r}, {tail_l, tail_r}};
+uint8_t joint_pins[NUM_JOINTS][2] = {{head_l, head_r}, {joint2_l, joint2_r}, {joint3_l, joint3_r}, {tail_l, tail_r}};
 uint8_t LED = 13;
 
 void setup()
 {
   // ARM MOTORS
-  pinMode(fin_l, OUTPUT);
-  pinMode(fin_r, OUTPUT);
+  pinMode(tail_l, OUTPUT);
+  pinMode(tail_r, OUTPUT);
   pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 }
 
 
@@ -26,6 +32,9 @@ void loop()
 {
   static uint32_t last_changed = 0;
   if (millis() - last_changed > 500) {
+    Serial.print("Joint: ");
+    Serial.print(joint_to_move);
+    Serial.print("\n");
     moveNextJoint();
     last_changed = millis();
     joint_to_move = (joint_to_move + 1) % NUM_JOINTS; // make it loop back from 3 to 0.
@@ -51,12 +60,12 @@ void flap(int dir, int duty) {
 
 void flapFin(int dir) {
   if (dir == RIGHT) {
-    digitalWrite(fin_r, HIGH);
-    digitalWrite(fin_l, LOW);
+    digitalWrite(tail_r, HIGH);
+    digitalWrite(tail_l, LOW);
     digitalWrite(LED, HIGH);
   } else {
-    digitalWrite(fin_r, LOW);
-    digitalWrite(fin_l, HIGH);
+    digitalWrite(tail_r, LOW);
+    digitalWrite(tail_l, HIGH);
     digitalWrite(LED, LOW);
   }
 }
@@ -65,10 +74,15 @@ void moveNextJoint() {
   // Assuming that all joint movement right now is just bang bang
   // in the opposite direction of where they were
   // Get current joint direction
-  int curr_dir = (joint_directions >> joint_to_move) && 1;
+  int curr_dir = (joint_directions >> joint_to_move) & 1;
+  
   // flap fin in opposite direction, currently at full amplitude
   flap(1 - curr_dir, 100);
   // update the joint's direction. Current joint to move is updated in loop.
-  joint_directions = joint_directions ^ (uint8_t)pow(2, joint_to_move);
+  Serial.print("XOR With: ");
+  Serial.print((int)(pow(2, joint_to_move) + 0.5), BIN);
+  Serial.print("\n");
+  joint_directions = joint_directions ^ (uint8_t)(pow(2, joint_to_move)+0.5);
+  Serial.println(joint_directions, BIN);
 }
 
