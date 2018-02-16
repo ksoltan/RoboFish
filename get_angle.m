3d9577186d6af6c66ccbb5150dc5eefb7834727b
Sean Foley <sean.foley@newgridinc.com>
2018-02-16 16:24:12 -0500

    Cleaning out directory for this branch.

--- get_angle.m
+++
@@ -1,9 +0,0 @@
-% Get angle between two points that define a vector.
-% Angle defined with respect to +x axis. Angles from -180 < theta < 180
-function angle = get_angle(pt1, pt2)
-    pt1_x = pt1(1);
-    pt1_y = pt1(2);
-    pt2_x = pt2(1);
-    pt2_y = pt2(2);
-    angle = atan2d((pt2_y - pt1_y),(pt2_x - pt1_x)); % atan2d returns -180 < theta < 180 and takes care of vertical alignment
-end
--- get_posture.m
+++
@@ -1,7 +0,0 @@
-function y = get_posture(x, t)
-    c1 = 0.1; % Amplitude of oscillation. The highe c1 is, the more wide the sweep is of the function
-    c2 = 0; % When not 0, introduces quadratic term. Motion is more steep and tightly curved
-    k = 0.05; % Frequency of oscillation. The higher k is, the more zig-zaggy the motion is
-    omega = 20; % Speed of oscillation. The higher omega is, the faster the body moves
-    y = (c1 .* x + c2 .* x.^2) .* sin(k .* x + omega .* t) - c1 .* x .* sin(omega .* t);
-end
